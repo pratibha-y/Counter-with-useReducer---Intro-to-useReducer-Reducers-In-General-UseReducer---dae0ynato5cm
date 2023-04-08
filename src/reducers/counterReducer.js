618187508b78340622313a71e83c35a25254cb90
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-09 00:00:20 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,17 @@
-const counterReducer = (state,action) =>{
+/*const counterReducer = (state,action) =>{
 
+}
+
+export {counterReducer}*/
+const counterReducer = (state,action) =>{
+    switch(action){
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
 }
 
 export {counterReducer}
